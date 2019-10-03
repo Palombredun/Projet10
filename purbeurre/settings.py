6a995a64aed0c458ab67fe67e28d20328f01da9d
baptiste <baptiste.name>
2019-10-03 16:20:33 +0200

    modification settings.py

--- purbeurre/settings.py
+++ purbeurre/settings.py
@@ -15,7 +15,7 @@ import os
 import purbeurre.production as prod
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+#BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
 # Quick-start development settings - unsuitable for production
@@ -84,12 +84,7 @@ WSGI_APPLICATION = "purbeurre.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
-    }
-}
+DATABASES =prod.DATABASES
 
 
 # Password validation
